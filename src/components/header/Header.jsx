daccc4176a61ee2950d4723962370a02796faa6d
akoobeeEy <119799055+akoobeeEy@users.noreply.github.com>
2023-08-31 18:43:06 +0700

    Update Header.jsx

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,7 +4,7 @@ import Nav from "./nav/Nav";
 
 const Header = () => {
   return (
-    <header className="fixed top-0 z-50 w-full py-4">
+    <header className="sticky top-0 z-50 w-full py-4">
       <div className="container mx-auto" >
         <Nav/>
       </div>
